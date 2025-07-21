bcb8e803b01f75a9ae8d4dd524d19b30d9de7278
novalwardhana <novalita.k.wardhana@gmail.com>
2025-07-21 12:14:07 +0700

    simple httprouter

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
 
 func main() {
-	fmt.Println("Hello world")
+
+	router := httprouter.New()
+	router.GET("/", func(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+		writer.WriteHeader(http.StatusOK)
+		fmt.Fprint(writer, "Hello world")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+
 }
